e5f4f7ce65bdeae973b0173b40f4c3b778dbf266
BarrosAmorim <barrosamorimd@gmail.com>
2021-08-29 19:41:21 -0300

    Comentarios

--- main.py
+++ main.py
@@ -29,54 +29,54 @@ def menu():
         
         voltarMenu = input("Deseja voltar ao menu principal ? (s/n) ").lower()
    
-def cadastrarContato():
-    nome = input('Nome: ')
+def cadastrarContato(): # Função cadastrar um novo contato
+    nome = input('Nome: ') # variaveis que vão armazenar dados informados pelo usuario
     phone = input('Telefone: ')
     email = input('E-mail: ')
     twitter = input('Twitter: ')
     facebook = input('Facebook: ')
     instagram = input('Instagram: ')
     try:
-        agenda = open('agenda.csv', 'a')
+        agenda = open('agenda.csv', 'a') # criar arquivo csv em mode de escrita
         dados = f'Nome:{nome}, Telefone:{phone}, Email:{email}, Twitter:{twitter}, Facebook:{facebook}, Instagram:{instagram} \n' 
-        agenda.write(dados)
+        agenda.write(dados) # Escreve os dados da agenda ao qual foram armazenados da variavel "dados"
         agenda.close()
         print('Contato cadastrado com sucesso!')
     except:
         print('Error')
 
-def listarContatos():
-    agenda = open('agenda.csv', 'r')
-    for contato in agenda:
+def listarContatos(): # Função listar contatos
+    agenda = open('agenda.csv', 'r') # abre o arquivo csv em modo leitura
+    for contato in agenda: # percorre todos os elementos da da agenda
         print(contato)
     agenda.close()
 
-def deletarContato():
-    nomeDeletado = input('Nome: ').lower()
+def deletarContato():# Função deletar contatos
+    nomeDeletado = input('Nome: ').lower() # . lower converte cada caractere maiústo em minúscula
     agenda = open("agenda.csv", 'r')
     aux = []
     aux2= []
-    for i in agenda:
-        aux.append(i)
-    for i in range(0, len(aux)):
-        if nomeDeletado not in aux[i].lower():
-            aux2.append(aux[i])
-    agenda = open('agenda.csv', 'w')
-    for i in aux2:
+    for i in agenda: # percorre todos os indices no array de contatos
+        aux.append(i) # grava todos os indices e armazena na variavel i
+    for i in range(0, len(aux)): # percorre cada nome e conta a quantidade de contatos na agenda
+        if nomeDeletado not in aux[i].lower(): # verifica se os dados informados pelo usuario contem na variavel aux
+            aux2.append(aux[i]) # caso o nome informado exista na agenda, armazena na variavel
+    agenda = open('agenda.csv', 'w') # abre o o arquivo em modo de escrita
+    for i in aux2: 
         agenda.write(i)
     print(f'Contato deletado com sucesso')
  
-def buscarContato():
-    nome=input(f'Insira nome: ').upper()
+def buscarContato(): # Função buscar contatos
+    nome=input(f'Insira nome: ').upper() # .upper converte todos os caracteres minúsculos em uma string em caracteres maiúsculos
     agenda = open('agenda.csv','r')
     for contato in agenda:
-        if nome in contato.split(';')[0].upper():
+        if nome in contato.split(';')[0].upper(): # verifica se o nome informado esta na lista de contatos separando os com uma ,
             print(contato)
     agenda.close()
 
-def sair():
+def sair(): # Funcão para sair do programa
     exit()
 
-def main():
+def main(): # Função que carrefa o menu dentro da funcao principal
     menu()
 main()
